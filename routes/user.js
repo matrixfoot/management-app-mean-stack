5202671ba0c46c445bf2e0ae164a0bb4cde66506
omar <omar.kammoun@stb.com.tn>
2020-05-17 14:24:55 +0100

    rbac backend

--- routes/user.js
+++ routes/user.js
@@ -6,12 +6,12 @@ router.post('/signup', userController.signup);
 
 router.post('/login', userController.login);
 
-router.get('/user/:userId', userController.allowIfLoggedin, userController.getUser);
+router.get('/:userId', userController.allowIfLoggedin, userController.getUser);
 
-router.get('/users', userController.allowIfLoggedin, userController.grantAccess('readAny', 'profile'), userController.getUsers);
+router.get('/', userController.allowIfLoggedin, userController.grantAccess('readAny', 'profile'), userController.getUsers);
 
-router.put('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('updateAny', 'profile'), userController.updateUser);
+router.put('/:userId', userController.allowIfLoggedin, userController.grantAccess('updateAny', 'profile'), userController.updateUser);
 
-router.delete('/user/:userId', userController.allowIfLoggedin, userController.grantAccess('deleteAny', 'profile'), userController.deleteUser);
+router.delete('/:userId', userController.allowIfLoggedin, userController.grantAccess('deleteAny', 'profile'), userController.deleteUser);
 
 module.exports = router;
