e1eb2dcb668cc525a0456e260f2c4f839f7e6547
matrixfoot <kammounomarindus@gmail.com>
2020-05-20 09:03:58 +0100

    users list added

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect('mongodb+srv://matrixfoot:indus789456@cluster0-zghco.mongodb.ne
   
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization,x-access-token');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
@@ -29,10 +29,10 @@ mongoose.connect('mongodb+srv://matrixfoot:indus789456@cluster0-zghco.mongodb.ne
     })
   );
   app.use(async (req, res, next) => {
-    if (req.headers["x-access-token"]) {
+    if (req.headers["x-access-token"] ) {
       try {
         const accessToken = req.headers["x-access-token"];
-        const { userId, exp } = await jwt.verify(accessToken, process.env.JWT_SECRET);
+        const { userId, exp } = await jwt.verify(accessToken, 'RANDOM_TOKEN_SECRET');
         // If token has expired
         if (exp < Date.now().valueOf() / 1000) {
           return res.status(401).json({
